09634ca67e04921df3a92a0e97e45fd30730d5d7
Joe_Johnson <joe_johnson@wycliffe.org>
2018-07-30 16:53:19 -0400

    Video fullscreened & custom control, init.

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import Dimmerr from './components/Dimmer';
 import Vid from './components/Video';
 
-// console.log(quiz[0].questions[0]);
-// const q1 = quiz[0].questions[0].question;
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Vid /> 
+        <Vid />
         <div className="tests">
           <p>In Testing Mode</p>
         </div>
--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -2,7 +2,54 @@ import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 
 export default class Vid extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      playing: true,
+      played: 0,
+      volume: 0.8
+    }
+  }
+
+  onPlay = () => {
+    console.log('onPlay')
+    this.setState({ playing: true })
+  }
+  onPause = () => {
+    console.log('onPause')
+    this.setState({ playing: false })
+  }
+  playPause = () => {
+    this.setState({ playing: !this.state.playing })
+  }
+  onclicky = () => {
+    console.log('thesting this :)')
+    this.onPause();
+  }
+  ref = player => {
+    this.player = player
+  }
+
   render () {
-    return <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
+    const { playing, volume, played } = this.state
+    const SEPARATOR = ' · '
+    console.log(this.state);
+    return (
+      <div className='player-wrapper'>
+        <ReactPlayer
+          ref={this.ref}
+          className='react-player'
+          playing={playing}
+          volume={volume}
+          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+          width='100%'
+          height='100vh'
+          onPlay={this.onPlay}
+          onPause={this.onPause}
+        />
+        <h1 onClick={this.playPause}>{this.state.playing ? 'Pause' : 'Play'}</h1>
+        <button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</button>
+      </div>
+    )
   }
 }
