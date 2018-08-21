564b1a952a4557b22e16a660485fe30b937941c5
Joe_Johnson <joe_johnson@wycliffe.org>
2018-08-21 13:58:42 -0400

    Home background styling.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -24,7 +24,7 @@ export default class Home extends Component {
   render(){
     const { visible } = this.state;
     return(
-      <div className="tests">
+      <div className="b-container">
         <div className="center">
           <h1 className="home-title">Why Bible Translation?</h1>
           <Transition visible={visible} animation='pulse' duration={200}>
