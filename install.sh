ab64fcebacf223c548c9ca6d512d15001fefd7b9
Snodgrass <COREY_SNODGRASS@wctnet.org>
2018-10-11 20:01:41 -0400

    Add scripts for copy video and installing app

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-# A shell script to copy a new video file into the project and run the
+# A shell script to run the build
 # build command.
 # Written by: Corey Snodgrass
 # Last updated on: 10/10/2018
 
-while [ -n "$1" ]; 
-if [ -e /dev/stdin ]; then
-  echo "Copying video file into directory"
-  sudo cp /dev/stdin /home/pi/mern-discovery-videoplayer/client/src/components/
-  echo "Changing Directories"
-  cd /home/pi/mern-discovery-videoplayer/
-  sudo npm run build
-else
-  echo "File does not exist"
-fi
+echo "Installing server..."
+npm install
+cd client/
+echo "Installing client..."
+npm install
+echo "Building app..."
+npm run live
